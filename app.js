6668fb2c29dc9fe321fa915dc9bc32153dde8c1d
Leslie <lesliezsy12@gmail.com>
2021-06-06 14:23:34 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const exphbs = require('express-handlebars')
 const { urlencoded } = require("body-parser")
 const methodOverride = require('method-override')
 const routes = require('./routes')
-// const usePassport = require('./config/passport')
 
 require('./config/mongoose')
 
@@ -16,8 +15,6 @@ app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }))
-
-// usePassport(app)
 
 app.set('view engine', 'hbs')
 
@@ -31,6 +28,14 @@ app.use(urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+// 使用 app.use 代表這組 middleware 會作用於所有的路由
+app.use((req, res, next) => {
+  // req.user 是在反序列化時，取出的 user 資訊，之後會放在 req.user 裡以供後續使用
+  console.log("req user: ",req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 app.use(express.static('public'))
 
