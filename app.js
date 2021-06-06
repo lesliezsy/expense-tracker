4a8908a1c5130b0b7412738cbb070c944f5a9b86
Leslie <lesliezsy12@gmail.com>
2021-06-06 13:17:34 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const session = require('express-session')
+// 載入passport設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const { urlencoded } = require("body-parser")
 const methodOverride = require('method-override')
@@ -27,6 +29,8 @@ app.use(session({
 
 app.use(urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 app.use(routes)
 app.use(express.static('public'))
 
