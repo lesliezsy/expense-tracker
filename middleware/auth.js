cec2724758ee42a472bbf30811e7a95633fa987e
Leslie <lesliezsy12@gmail.com>
2021-06-06 13:44:12 +0800

    feat: add user authentication in records and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // 匯出一個物件，物件裡是一個叫做 authenticator 的函式
+  authenticator: (req, res, next) => {
+    // req.isAuthenticated() 是 Passport.js 提供的函式，根據 request 的登入狀態回傳 true 或 false
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/users/login')
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
 
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+
 router.use('/records', records)
 router.use('/users', users)
 router.use('/', home)
